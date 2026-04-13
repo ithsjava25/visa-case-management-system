77e4e58695cc292b3202f8bf8176951eb95abef8
Ebba Andersson <eeebbaandersson@gmail.com>
2026-04-13 19:13:30 +0200

    test: extend test suite for VisaServiceTest

--- src/test/java/org/example/visacasemanagementsystem/visa/VisaServiceTest.java
+++ src/test/java/org/example/visacasemanagementsystem/visa/VisaServiceTest.java
@@ -8,6 +8,7 @@ import org.example.visacasemanagementsystem.user.UserAuthorization;
 import org.example.visacasemanagementsystem.user.entity.User;
 import org.example.visacasemanagementsystem.user.repository.UserRepository;
 import org.example.visacasemanagementsystem.visa.dto.CreateVisaDTO;
+import org.example.visacasemanagementsystem.visa.dto.UpdateVisaDTO;
 import org.example.visacasemanagementsystem.visa.entity.Visa;
 import org.example.visacasemanagementsystem.visa.mapper.VisaMapper;
 import org.example.visacasemanagementsystem.visa.repository.VisaRepository;
@@ -39,7 +40,7 @@ class VisaServiceTest {
     @InjectMocks
     private VisaService visaService;
 
-    // Alla find metoder?
+    // Find metoder?
 
     // applyForVisa 3x
     @Test
@@ -47,7 +48,7 @@ class VisaServiceTest {
         // Arrange
         Long userId = 1L;
         CreateVisaDTO dto = new CreateVisaDTO(
-                VisaType.STUDY, "Swedish","AB123",
+                VisaType.STUDY, "Swedish", "AB123",
                 LocalDate.now(),
                 userId
         );
@@ -62,11 +63,11 @@ class VisaServiceTest {
         when(visaRepository.save(any(Visa.class))).thenAnswer(i -> i.getArgument(0));
 
         // Act
-        visaService.applyForVisa(dto,userId);
+        visaService.applyForVisa(dto, userId);
 
         // Assert
         verify(visaRepository, times(1)).save(any(Visa.class));
-        verify(auditService, times(1)).createAuditLog(eq(userId), any(), any(),any());
+        verify(auditService, times(1)).createAuditLog(eq(userId), any(), any(), any());
         assertThat(visa.getVisaStatus()).isEqualTo(VisaStatus.SUBMITTED);
         assertThat(visa.getApplicant()).isEqualTo(user);
     }
@@ -76,14 +77,14 @@ class VisaServiceTest {
         // Arrange
         Long userId = 1L;
         CreateVisaDTO dto = new CreateVisaDTO(
-                VisaType.STUDY, "Swedish","AB123",
+                VisaType.STUDY, "Swedish", "AB123",
                 LocalDate.now().minusDays(1),
                 userId
         );
 
         // Act & Assert
-        assertThatThrownBy(()-> visaService.applyForVisa(dto, userId))
-        .isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> visaService.applyForVisa(dto, userId))
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Travel date cannot be in the past.");
 
         // Verify
@@ -95,7 +96,7 @@ class VisaServiceTest {
     void applyForVisa_shouldThrowException_WhenUserIsNotFoundInDb() {
         Long userId = 99L;
         CreateVisaDTO dto = new CreateVisaDTO(
-                VisaType.STUDY, "Swedish","AB123",
+                VisaType.STUDY, "Swedish", "AB123",
                 LocalDate.now(),
                 userId
         );
@@ -103,14 +104,129 @@ class VisaServiceTest {
         when(userRepository.findById(userId)).thenReturn(Optional.empty());
 
         // Act & Assert
-        assertThatThrownBy(()-> visaService.applyForVisa(dto, userId))
-        .isInstanceOf(EntityNotFoundException.class)
+        assertThatThrownBy(() -> visaService.applyForVisa(dto, userId))
+                .isInstanceOf(EntityNotFoundException.class)
                 .hasMessage("User not found");
     }
 
-    // approveVisa_shouldApproveVisa_andCreateLog
+    // updateVisa 3x
+    @Test
+    void updateVisa_shouldUpdateVisaAndResetStatus_WhenUserIsAuthorized() {
+        // Arrange
+        Long userId = 1L;
+        Long visaId = 100L;
 
+        UpdateVisaDTO dto = new UpdateVisaDTO(
+                visaId, VisaType.TOURIST, VisaStatus.SUBMITTED,
+                "Swedish", "XYZ789", LocalDate.now().plusMonths(1), null
+        );
 
+        User applicant = new User();
+        applicant.setId(userId);
+
+        Visa visa = new Visa();
+        visa.setId(visaId);
+        visa.setApplicant(applicant);
+        visa.setVisaStatus(VisaStatus.INCOMPLETE);
+        visa.setStatusInformation("PLease clarify travel purpose");
+
+        when(visaRepository.findById(visaId)).thenReturn(Optional.of(visa));
+        when(visaRepository.save(any(Visa.class))).thenAnswer(i -> i.getArgument(0));
+
+        // Act
+        visaService.updateVisa(dto, userId);
+
+        // Assert
+        assertThat(visa.getVisaType()).isEqualTo(VisaType.TOURIST);
+        assertThat(visa.getPassportNumber()).isEqualTo("XYZ789");
+        assertThat(visa.getVisaStatus()).isEqualTo(VisaStatus.SUBMITTED);
+        assertThat(visa.getStatusInformation()).isNull();
+
+        verify(auditService).createAuditLog(eq(userId), eq(visaId), eq(AuditEventType.UPDATED), anyString());
+
+    }
+
+    @Test
+    void updateVisa_shouldThrowUnauthorizedException_WhenUserIsNotTheApplicant() {
+        // Arrange
+        Long actualApplicantId = 3L;
+        Long unauthorizedUserId = 99L;
+
+        UpdateVisaDTO dto = new UpdateVisaDTO(100L, VisaType.EMPLOYMENT,
+                VisaStatus.SUBMITTED, "US", "123",
+                LocalDate.now().plusDays(1), null);
+
+        User actualApplicant = new User();
+        actualApplicant.setId(actualApplicantId);
+
+        Visa visa = new Visa();
+        visa.setApplicant(actualApplicant);
+
+        when(visaRepository.findById(anyLong())).thenReturn(Optional.of(visa));
+
+        // Act & Assert
+        assertThatThrownBy(() -> visaService.updateVisa(dto, unauthorizedUserId))
+                .isInstanceOf(UnauthorizedException.class)
+                .hasMessage("You are not authorized to update this application.");
+
+    }
+
+    @Test
+    void updateVisa_shouldThrowIllegalArgumentException_WhenTravelDateIsInPast() {
+        // Arrange
+        Long userId = 1L;
+        LocalDate pastDate = LocalDate.now().minusDays(1);
+
+        UpdateVisaDTO dto = new UpdateVisaDTO(100L, VisaType.STUDY,
+                VisaStatus.SUBMITTED, "Swedish", "CDE789", pastDate, null);
+
+        User actualUser = new User();
+        actualUser.setId(userId);
+
+        Visa visa = new Visa();
+        visa.setApplicant(actualUser);
+
+        when(visaRepository.findById(anyLong())).thenReturn(Optional.of(visa));
+
+        // Act & Assert
+        assertThatThrownBy(() -> visaService.updateVisa(dto, userId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Travel date cannot be in the past.");
+
+    }
+
+    @Test
+    void approveVisa_shouldApproveVisa_andCreateLog() {
+        // Arrange
+        Long visaId = 1L;
+        Long adminId = 2L;
+
+        User admin = new User();
+        admin.setId(adminId);
+        admin.setUserAuthorization(UserAuthorization.ADMIN);
+
+        Visa visa = new Visa();
+        visa.setId(visaId);
+        visa.setVisaStatus(VisaStatus.SUBMITTED);
+        visa.setApplicant(null);
+
+        when(userRepository.findById(adminId)).thenReturn(Optional.of(admin));
+        when(visaRepository.findById(visaId)).thenReturn(Optional.of(visa));
+        when(visaRepository.save(any(Visa.class))).thenAnswer(i -> i.getArgument(0));
+
+        // Act
+        visaService.approveVisa(visaId, adminId);
+
+        // Assert
+        assertThat(visa.getVisaStatus()).isEqualTo(VisaStatus.GRANTED);
+        assertThat(visa.getStatusInformation()).isNull();
+        assertThat(visa.getHandler()).isEqualTo(admin);
+
+        verify(visaRepository, times(1)).save(visa);
+        verify(auditService).createAuditLog(eq(adminId), eq(visaId), eq(AuditEventType.GRANTED),
+                contains("granted"));
+
+    }
 
     // rejectVisa 2x
     @Test
@@ -156,8 +272,69 @@ class VisaServiceTest {
         verifyNoInteractions(auditService);
     }
 
-    // requestMoreInformation_shouldUpdateStatusAndCreateLog
-    // assignHandler_shouldAssignHandlerToVisaCase_AndCreateLog
+    @Test
+    void requestMoreInformation_shouldUpdateStatusAndInfoText_AndCreateLog() {
+        // Arrange
+        Long visaId = 1L;
+        Long adminId = 2L;
+        String infoText = "Please upload a clearer picture of your passport";
+
+        User admin = new User();
+        admin.setUserAuthorization(UserAuthorization.ADMIN);
+
+        Visa visa = new Visa();
+        visa.setVisaStatus(VisaStatus.SUBMITTED);
+        visa.setHandler(null);
+
+        when(userRepository.findById(adminId)).thenReturn(Optional.of(admin));
+        when(visaRepository.findById(visaId)).thenReturn(Optional.of(visa));
+        when(visaRepository.save(any())).thenAnswer(i -> i.getArgument(0));
+
+        // Act
+        visaService.requestMoreInformation(visaId, adminId, infoText);
+
+        // Assert
+        assertThat(visa.getVisaStatus()).isEqualTo(VisaStatus.INCOMPLETE);
+        assertThat(visa.getStatusInformation()).isEqualTo(infoText);
+        assertThat(visa.getHandler()).isEqualTo(admin);
+
+        verify(auditService).createAuditLog(
+                eq(adminId),
+                eq(visaId),
+                eq(AuditEventType.UPDATED),
+                contains(infoText));
+    }
+
+    @Test
+    void assignHandler_shouldAssignAdminToVisa_AndChangeStatusToAssigned() {
+        // Arrange
+        Long visaId = 1L;
+        Long adminId = 2L;
+
+        User  admin = new User();
+        admin.setFullName("Test Admin");
+        admin.setUserAuthorization(UserAuthorization.ADMIN);
+
+        Visa visa = new Visa();
+        visa.setVisaStatus(VisaStatus.SUBMITTED);
+
+        when(userRepository.findById(adminId)).thenReturn(Optional.of(admin));
+        when(visaRepository.findById(visaId)).thenReturn(Optional.of(visa));
+        when(visaRepository.save(any())).thenAnswer(i -> i.getArgument(0));
+
+        // Act
+        visaService.assignHandler(visaId, adminId);
+
+        // Assert
+        assertThat(visa.getHandler()).isEqualTo(admin);
+        assertThat(visa.getVisaStatus()).isEqualTo(VisaStatus.ASSIGNED);
+
+        verify(auditService).createAuditLog(
+                eq(adminId),
+                eq(visaId),
+                eq(AuditEventType.ASSIGNED),
+                contains("Test Admin"));
+    }
 
     // ValidateHandler 3x
     @Test
@@ -183,8 +360,8 @@ class VisaServiceTest {
         applicant.setUserAuthorization(UserAuthorization.USER);
         when(userRepository.findById(userId)).thenReturn(Optional.of(applicant));
 
-        assertThatThrownBy(()-> visaService.validateHandler(userId))
-                    .isInstanceOf(UnauthorizedException.class)
+        assertThatThrownBy(() -> visaService.validateHandler(userId))
+                .isInstanceOf(UnauthorizedException.class)
                 .hasMessageContaining("User is not authorized to perform this action.");
     }
 
@@ -196,15 +373,12 @@ class VisaServiceTest {
         when(userRepository.findById(nonExistingUserId)).thenReturn(Optional.empty());
 
         // Act & Assert
-        assertThatThrownBy(()-> visaService.validateHandler(nonExistingUserId))
-        .isInstanceOf(EntityNotFoundException.class)
+        assertThatThrownBy(() -> visaService.validateHandler(nonExistingUserId))
+                .isInstanceOf(EntityNotFoundException.class)
                 .hasMessage("User not found.");
 
         verifyNoInteractions(visaRepository);
     }
-
-
-
 
 
 }
