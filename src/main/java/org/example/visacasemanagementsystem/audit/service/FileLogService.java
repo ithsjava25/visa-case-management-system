b94db7e637121178c3ee5ea30f1d691827781db8
Ebba Andersson <eeebbaandersson@gmail.com>
2026-04-24 09:55:51 +0200

    feat: implement comment-/file repos and services

---
+++ src/main/java/org/example/visacasemanagementsystem/audit/service/FileLogService.java
@@ -0,0 +1,40 @@
+package org.example.visacasemanagementsystem.audit.service;
+
+import org.example.visacasemanagementsystem.audit.CommentEventType;
+import org.example.visacasemanagementsystem.audit.FileEventType;
+import org.example.visacasemanagementsystem.audit.UserEventType;
+import org.example.visacasemanagementsystem.audit.dto.FileLogDTO;
+import org.example.visacasemanagementsystem.audit.dto.UserLogDTO;
+import org.example.visacasemanagementsystem.audit.entity.CommentLog;
+import org.example.visacasemanagementsystem.audit.entity.FileLog;
+import org.example.visacasemanagementsystem.audit.entity.UserLog;
+import org.example.visacasemanagementsystem.audit.mapper.FileLogMapper;
+import org.example.visacasemanagementsystem.audit.repository.FileLogRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FileLogService {
+
+    private final FileLogRepository fileLogRepository;
+    private final FileLogMapper fileLogMapper;
+
+    public FileLogService(FileLogRepository fileLogRepository, FileLogMapper fileLogMapper) {
+        this.fileLogRepository = fileLogRepository;
+        this.fileLogMapper = fileLogMapper;
+    }
+
+    public void createFileLog(Long actorUserId, Long visaCasId, String fileName, FileEventType fileEventType, String description) {
+        FileLog fileLog = fileLogMapper.toEntity(actorUserId, visaCasId, fileName, fileEventType, description);
+        fileLogRepository.save(fileLog);
+
+    }
+
+    public List<FileLogDTO> findAll() {
+        return fileLogRepository.findAll()
+                .stream()
+                .map(fileLogMapper::toDTO)
+                .toList();
+    }
+}
