90babe3c963fd1de1abb7b84fd7330f6405a7e3f
Edvin Sandgren <229709012+EdvinSandgren@users.noreply.github.com>
2026-04-27 16:10:04 +0200

    Spotless fix

--- src/main/java/org/example/visacasemanagementsystem/user/dto/UpdateUserDTO.java
+++ src/main/java/org/example/visacasemanagementsystem/user/dto/UpdateUserDTO.java
@@ -7,8 +7,6 @@ import jakarta.validation.constraints.Pattern;
 public record UpdateUserDTO(
         @NotNull Long id,
         @NotBlank(message = "Full name must be specified") String fullName,
-
         @NotNull(message = "Password must be provided (use empty string to skip change)")
-        @Pattern(regexp = "^$|.{8,}", message = "Password must be at least 8 characters")
-        String password) {
+        @Pattern(regexp = "^$|.{8,}", message = "Password must be at least 8 characters") String password) {
 }
